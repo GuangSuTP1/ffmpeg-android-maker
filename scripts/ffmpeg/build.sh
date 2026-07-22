98831d4aee151f9e1c0c2f33624305b8a738658c
不想上学的蛋糕 <164712020+GuangSuTP1@users.noreply.github.com>
2026-07-22 23:36:38 +0800

    Update linker flags for FFMPEG build script

--- scripts/ffmpeg/build.sh
+++ scripts/ffmpeg/build.sh
@@ -27,7 +27,7 @@ DEP_LD_FLAGS="-L${BUILD_DIR_EXTERNAL}/${ANDROID_ABI}/lib $FFMPEG_EXTRA_LD_FLAGS"
 
 # Android 15 with 16 kb page size support
 # https://developer.android.com/guide/practices/page-sizes#compile-r27
-EXTRA_LDFLAGS="-Wl,-z,max-page-size=16384 $DEP_LD_FLAGS -static-libgcc -static-libstdc++"
+EXTRA_LDFLAGS="-Wl,-z,max-page-size=16384 $DEP_LD_FLAGS -Wl,--allow-multiple-definition"
 
 ./configure \
   --prefix=${BUILD_DIR_FFMPEG}/${ANDROID_ABI} \
